53b7ef7c960a36de7350a02de4789e7ea74301f3
Madison Rideout <mlride3551@ung.edu>
2018-10-22 17:20:52 -0400

    Update

--- app/src/main/java/imedtracker/csci3300/csci/mccb/ung/edu/imedtracker/SettingsController.java
+++ app/src/main/java/imedtracker/csci3300/csci/mccb/ung/edu/imedtracker/SettingsController.java
@@ -8,5 +8,6 @@ public class SettingsController extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_view);
+        setTitle("Settings");
     }
 }
